d785e5426ec20f931e8e314abbb0cc8c5b8435d9
Yuvaan1182 <hw9044@gmail.com>
2021-03-24 18:46:31 +0530

    added fragment and set clicklistener on navitem to toggle b/w fragments

--- app/src/main/java/com/example/instagramclone/MainActivity.kt
+++ app/src/main/java/com/example/instagramclone/MainActivity.kt
@@ -6,35 +6,73 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationMenu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var bottomNavBar: BottomNavigationView
+
+    val manager = supportFragmentManager
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         bottomNavBar = findViewById(R.id.bottom_navigation_bar)
+
+        val homeFragment = HomeFragment()
+        val favouriteFragment = FavouriteFragment()
+        val searchFragment = SearchFragment()
+        val shareFragment = ShareFragment()
+        val profileFragment = ProfileFragment()
+
+        manager.beginTransaction().apply {
+            replace(R.id.fragment, homeFragment)
+            commit()
+        }
+
         bottomNavBar.setOnNavigationItemSelectedListener { item->
             when(item.itemId){
                 R.id.home->{
+                    manager.beginTransaction().apply {
+                        replace(R.id.fragment, homeFragment)
+                        commit()
+                    }
                     true
                 }
                 R.id.search->{
+                    manager.beginTransaction().apply {
+                        replace(R.id.fragment, searchFragment)
+                        commit()
+                    }
                     true
                 }
 
                 R.id.add->{
+                    manager.beginTransaction().apply {
+                        replace(R.id.fragment, shareFragment)
+                        commit()
+                    }
                     true
                 }
 
                 R.id.favourite->{
+                    manager.beginTransaction().apply {
+                        replace(R.id.fragment, favouriteFragment)
+                        commit()
+                    }
                     true
                 }
 
                 R.id.user->{
+                    manager.beginTransaction().apply {
+                        replace(R.id.fragment, profileFragment)
+                        commit()
+                    }
                     true
                 }
                 else -> {
@@ -50,6 +88,5 @@ class MainActivity : AppCompatActivity() {
         inflater.inflate(R.menu.bottom_navigation_menu, menu)
         return true
     }
-
 
 }
